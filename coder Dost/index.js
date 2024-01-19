40c7301cb0f2a6975537667fa122f8c58a33c96e
abhijitholi <jitholi83@gmail.com>
2024-01-19 11:24:50 +0530

    18 01 2024

---
+++ coder Dost/index.js
@@ -0,0 +1,11 @@
+
+const fs = require('node:fs');
+
+
+const t1= performance.now(); 
+fs.readFile('demo.txt', 'utf-8',(err, txt)=>{
+    console.log(txt);
+});
+const t2= performance.now(); 
+console.log('hiii')
+console.log(t2-t1);
---
+++ coder Dost/test.js
@@ -0,0 +1,8 @@
+
+
+    exports.diff = (a,b) => {
+        return a-b;
+    };
+    exports.diff2 = () => {
+        console.log('hii');
+    }

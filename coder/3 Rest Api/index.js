20522f6e94a5e1b2970338d5ff90c039eeee688a
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-03-09 06:57:08 +0530

    9 mar 2024

--- coder/3 Rest Api/index.js
+++ coder/3 Rest Api/index.js
@@ -1,14 +1,45 @@
 const express = require('express')
 const app = express();
-const port = 8080;
+const port = 3005;
 
 const products = require('./data.json');
 
+// body Parser
+app.use(express.json());
+
 // Endpoins
+
+// Read GET /products
 app.get('/products',(req,res)=>{
     //res.send('Hii')
     res.json(products)
 })
+
+app.get('/products/:id',(req,res)=>{
+    //res.send('Hii')
+    console.log(req.params.id)
+    const id = +req.params.id;
+   const Product = products.find(p=>p.id===id)
+    res.json(Product)
+})
+
+app.get('/products/Api/:postId',(req,res)=>{
+    //res.send('Hii')
+    console.log(req.params.postId)
+    const postId = +req.params.postId;
+   const Product = products.find(p=>p.postId===postId)
+    res.json(Product)
+})
+
+app.post('/products/',(req,res)=>{
+    //res.send('Hii')
+    console.log(req.body)
+    res.json({type:'POST'})
+   
+    
+})
+
+ 
 
 app.listen(port,()=>{
     console.log(`server is started on https://localhost:${port}`)
